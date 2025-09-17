5a57bfd23d13bd3d7b2c2a9d90925b2c57ec42fb
karinahurzan <kgurzan@gmail.com>
2025-09-17 18:39:14 +0300

    upload cliend components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import StatusLabel, { Status } from '@/app/components/StatusLabel';
+import { headers } from 'next/headers';
+import AddCompanyButton from './components/AddCompanyModal';
 
 export default function Home() {
+  console.log(headers());
+
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <AddCompanyButton />
     </main>
   );
 }
